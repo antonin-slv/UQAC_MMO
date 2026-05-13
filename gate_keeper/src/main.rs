3f3eebf9c01d607c769b006f4ee0ce4402665df7
Otchi <adamgragnic@gmail.com>
2026-05-13 17:36:30 -0400

    Add base database management and authentification

--- gate_keeper/src/main.rs
+++ gate_keeper/src/main.rs
@@ -1,39 +1,22 @@
 #[macro_use]
 extern crate rocket;
+mod database_manager;
+mod rocket_manager;
 
+use crate::database_manager::DatabaseManager;
+use crate::rocket_manager::RocketManager;
 use dotenv::dotenv;
-use std::env;
+use std::sync::Arc;
 
-#[launch]
-fn rocket() -> _ {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     dotenv().ok();
 
-    rocket::build().mount("/gate-keeper", routes![login])
-}
+    let database_manager = Arc::new(DatabaseManager::new().await?);
 
-#[post("/login")]
-async fn login() -> String {
-    let gatekeeper_address =
-        &env::var("GATEKEEPER_ADDRESS").expect("Env GATEKEEPER_ADDRESS is not set");
-    let gatekeeper_port: u16 = env::var("GATEKEEPER_PORT")
-        .expect("Env GATEKEEPER_PORT is not set")
-        .parse()
-        .expect("Env GATEKEEPER_PORT is not a valid number");
+    tokio::spawn(async move {
+        RocketManager::new(database_manager).await;
+    });
 
-    let orchestrator_address = format!(
-        "http://{}:{}/orchestrator",
-        gatekeeper_address, gatekeeper_port
-    );
-    let response = reqwest::get(format!("{}/connect", orchestrator_address)).await;
-
-    if let Ok(response) = response
-        && response.status().is_success()
-    {
-        let body = response.text().await;
-        if let Ok(body) = body {
-            return body;
-        }
-    }
-
-    "Orchestrator failed to login.".to_string()
+    loop {}
 }
