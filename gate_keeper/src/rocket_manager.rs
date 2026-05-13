662c132e774185c98117a30393c7dc98533f41de
Otchi <adamgragnic@gmail.com>
2026-05-13 18:13:49 -0400

    Add health and status on request

--- gate_keeper/src/rocket_manager.rs
+++ gate_keeper/src/rocket_manager.rs
@@ -1,6 +1,9 @@
 use crate::database_manager::DatabaseManager;
+use rocket::http::Status;
+use rocket::response::{content, status};
 use rocket::serde::{Deserialize, json::Json};
 use rocket::{Ignite, Rocket, State};
+use serde::Serialize;
 use std::env;
 use std::sync::Arc;
 
@@ -17,7 +20,7 @@ impl RocketManager {
 
         let rocket = rocket::build()
             .manage(database)
-            .mount("/gate-keeper", routes![login, register])
+            .mount("/gate-keeper", routes![login, register, health])
             .configure(rocket::Config::figment().merge(("port", gatekeeper_port)))
             .launch()
             .await
@@ -33,39 +36,35 @@ struct Login {
     password: String,
 }
 
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct ServerInfo {
+    ip: String,
+    port: u16,
+    zone: String,
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct LoginResponse {
+    player_id: String,
+    server: ServerInfo,
+}
+
 #[post("/login", data = "<login>")]
-async fn login(database: &State<Arc<DatabaseManager>>, login: Json<Login>) -> String {
+async fn login(
+    database: &State<Arc<DatabaseManager>>,
+    login: Json<Login>,
+) -> Result<Json<LoginResponse>, Status> {
     let login = database
         .login(login.username.as_str(), login.password.as_str())
         .await;
 
-    if let Ok(login) = login
-        && login
-    {
-        let gatekeeper_address =
-            &env::var("ORCHESTRATOR_ADDRESS").expect("Env ORCHESTRATOR_ADDRESS is not set");
-        let gatekeeper_port: u16 = env::var("ORCHESTRATOR_PORT")
-            .expect("Env ORCHESTRATOR_PORT is not set")
-            .parse()
-            .expect("Env ORCHESTRATOR_PORT is not a valid number");
-
-        let orchestrator_address = format!(
-            "http://{}:{}/orchestrator",
-            gatekeeper_address, gatekeeper_port
-        );
-        let response = reqwest::get(format!("{}/connect", orchestrator_address)).await;
-
-        if let Ok(response) = response
-            && response.status().is_success()
-        {
-            let body = response.text().await;
-            if let Ok(body) = body {
-                return body;
-            }
-        }
+    if !login {
+        return Err(Status::Unauthorized);
     }
 
-    "Orchestrator failed to login.".to_string()
+    get_available_server().await
 }
 
 #[derive(Deserialize)]
@@ -76,10 +75,53 @@ struct Register {
 }
 
 #[post("/register", data = "<register>")]
-async fn register(database: &State<Arc<DatabaseManager>>, register: Json<Register>) -> String {
+async fn register(
+    database: &State<Arc<DatabaseManager>>,
+    register: Json<Register>,
+) -> Result<Json<LoginResponse>, Status> {
     let _ = database
         .register(register.username.as_str(), register.password.as_str())
         .await;
 
-    "Registered".to_string()
+    get_available_server().await
+}
+
+#[get("/health")]
+async fn health() -> content::RawJson<&'static str> {
+    content::RawJson("{ 'status': 'ok' }")
+}
+
+async fn get_available_server() -> Result<Json<LoginResponse>, Status> {
+    let gatekeeper_address =
+        &env::var("ORCHESTRATOR_ADDRESS").expect("Env ORCHESTRATOR_ADDRESS is not set");
+    let gatekeeper_port: u16 = env::var("ORCHESTRATOR_PORT")
+        .expect("Env ORCHESTRATOR_PORT is not set")
+        .parse()
+        .expect("Env ORCHESTRATOR_PORT is not a valid number");
+
+    let orchestrator_address = format!(
+        "http://{}:{}/orchestrator",
+        gatekeeper_address, gatekeeper_port
+    );
+    let response = reqwest::get(format!("{}/connect", orchestrator_address)).await;
+
+    if let Ok(response) = response
+        && response.status().is_success()
+    {
+        let body = response.text().await;
+        if let Ok(body) = body {
+            let response = LoginResponse {
+                player_id: "7db9b582-7771-4654-8e81-799f9c73e34b".to_string(), // Exemple d'UUID
+                server: ServerInfo {
+                    ip: body.to_string(),
+                    port: 7001,
+                    zone: "zone_A".to_string(),
+                },
+            };
+
+            return Ok(Json(response));
+        }
+    }
+
+    Err(Status::BadRequest)
 }
