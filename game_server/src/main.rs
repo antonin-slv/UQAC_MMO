27a2112a357cd5a98ea819c7356d25a811ff0043
Antonin <antonin.sylvestre@orange.fr>
2026-05-14 15:47:19 -0400

    server : proper reading of env variables

--- game_server/src/main.rs
+++ game_server/src/main.rs
@@ -1,43 +1,42 @@
-
-mod snapshot;
-mod network;
-mod game;
 mod events;
+mod game;
+mod network;
+mod snapshot;
 
-use bevy::prelude::*;
+use crate::game::GameLogicPlugin;
+use crate::network::NetworkPlugin;
 use bevy::app::{App, ScheduleRunnerPlugin};
-use std::time::Duration;
+use bevy::prelude::*;
 use dotenv::dotenv;
 use std::env;
+use std::time::Duration;
 
-use crate::network::{NetworkPlugin};
-use crate::game::GameLogicPlugin;
-
-const DEFAULT_SERV_FREQUENCY : u16 = 60;
+const DEFAULT_SERV_FREQUENCY: u16 = 60;
 const SERV_FREQUENCY_ENV_NAME: &str = "SERV_FREQUENCY";
-
 
 fn main() {
     dotenv().ok();
 
     let serv_frequency = env::var(SERV_FREQUENCY_ENV_NAME);
-    println!("Serving frequency from {:?}s...", serv_frequency);
     let serv_frequency = match serv_frequency {
-        Ok(freq) => freq.parse::<u16>().unwrap_or({
-            eprintln!("Error: SERV_FREQUENCY was not a valid u16. Defaulting to {} Hz.", DEFAULT_SERV_FREQUENCY);
+        Ok(freq) => freq.parse::<u16>().unwrap_or_else(|_| {
+            eprintln!("Error: SERV_FREQUENCY was not a valid u16");
             DEFAULT_SERV_FREQUENCY
         }),
         Err(_) => {
-            eprintln!("Error: SERV_FREQUENCY must be provided : Default to {} HZ", DEFAULT_SERV_FREQUENCY);
+            eprintln!("Error: SERV_FREQUENCY must be provided");
             DEFAULT_SERV_FREQUENCY
-        },
+        }
     };
+    println!("[Server] Server frequency: {} Hz", serv_frequency);
 
     // Lancement de Bevy (Boucle principale)
     App::new()
-        .add_plugins(MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(
-            Duration::from_secs_f64(1.0 / (serv_frequency as f64)),
-        )))
+        .add_plugins(
+            MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f64(
+                1.0 / (serv_frequency as f64),
+            ))),
+        )
         .add_plugins(NetworkPlugin)
         .add_plugins(GameLogicPlugin)
         .add_plugins(snapshot::SnapshotPlugin)
