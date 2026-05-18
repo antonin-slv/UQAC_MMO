d1864afb7fba5395ceee7719f93e683648e62f95
Otchi <adamgragnic@gmail.com>
2026-05-18 11:39:54 -0400

    Move gatekeeper to redis

--- gate_keeper/src/main.rs
+++ gate_keeper/src/main.rs
@@ -7,6 +7,7 @@ use crate::database_manager::DatabaseManager;
 use crate::rocket_manager::RocketManager;
 use dotenv::dotenv;
 use std::sync::Arc;
+use shared_replication::redis_manager::RedisManager;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -15,10 +16,11 @@ async fn main() -> anyhow::Result<()> {
     println!("Starting gatekeeper server...");
     println!("Connecting to database server...");
     let database_manager = Arc::new(DatabaseManager::new().await?);
+    let redis_manager = Arc::new(RedisManager::new().await?);
 
     tokio::spawn(async move {
         println!("Start rocket");
-        RocketManager::new(database_manager).await;
+        RocketManager::new(database_manager, redis_manager).await;
     });
 
     loop {}
